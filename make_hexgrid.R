c5d8f4437462b3368b2c1eb48c208778ebd62b72
chris-kerry <c.kerry@exeter.ac.uk>
2020-06-30 13:44:11 +0100

    back to test stage

--- make_hexgrid.R
+++ make_hexgrid.R
@@ -2,11 +2,7 @@
 library(rgdal)
 library(pracma)
 
-<<<<<<< Updated upstream
-test2 -> test1
-=======
-test <- test
->>>>>>> Stashed changes
+
 
 #point geometry shapefile(s)
 GPS <- readOGR(".", "cmax_lightgeo_wgs84")
